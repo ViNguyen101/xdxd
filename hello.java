0987de9326f776740f5d33f0ec11065f6e756b5e
ViNguyen101 <vi_nguyen101@hotmail.com>
2025-01-20 22:57:13 +1100

    wow

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
 class hello {
 	public static void main(String[] args){
-		System.out.println("Shixi is a sleepy woo");
+		System.out.println("Shixi is a sleepy wasdsadasadasdasdassdsoof");
+		System.out.println("Shixi is a sleepy wasdsadasadasdasdassdsoof");
+
 	}
 }
