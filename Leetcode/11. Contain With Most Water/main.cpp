fd57ccad06dc983af80796b12c3532374904b7ec
ViNguyen101 <vi_nguyen101@hotmail.com>
2025-01-23 23:42:04 +1100

    added?

---
+++ Leetcode/11. Contain With Most Water/main.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+class Solution {
+
+public:
+    int maxArea(vector<int>& height) {
+        int maxArea = 0;
+        int width = 0;
+        for(int i : height) {
+            cout<<i<<" ";
+
+            width
+        } 
+        return 0;
+    }
+};
