0596da4a949d91486515a5636338dd2b90427508
ViNguyen101 <vi_nguyen101@hotmail.com>
2025-01-24 01:21:45 +1100

    completed chall

--- Leetcode/11. Contain With Most Water/main.cpp
+++ Leetcode/11. Contain With Most Water/main.cpp
@@ -6,14 +6,34 @@ class Solution {
 
 public:
     int maxArea(vector<int>& height) {
-        int maxArea = 0;
+        int maxAreaThis = 0;
+        int z = 0;
+        int y = 0;
+        int track = 1;
         int width = 0;
-        for(int i : height) {
-            cout<<i<<" ";
-            //making changes
-            
+
+            //as we traverse the array, we can find the max area, lets assume that the current area we have so far is max area; theres 2 cases
+            //case 1; current area is less than our new area - we take the new points, but 
+            //case 2; current area is more than our new area - we disregard.
+        for(int i =0;i <height.size(); ++i) {
+
+            for (int j = width; j < height.size(); ++j) {
+                y = (height.at(i) < height.at(j)) ? height.at(i) : height.at(j);
+                z = (j - width) * y;
+                
+                
+                if(maxAreaThis < z) {
+
+                    maxAreaThis = z;
+                    cout << "Max = " << maxAreaThis << " z = " << (j - width) << " y = " << y  << " i = " << i  << " j = " << j  <<  endl;
+
+                }
+            }
+
+            ++width;
+
         } 
-        return 0;
+        return maxAreaThis;
     }
 
 
